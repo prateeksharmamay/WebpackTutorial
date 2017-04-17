61d8db73df5d6266437e43e91fe8fcc46f7a2790
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-16 22:14:22 -0700

    Pushing project with basic webpack concepts

---
+++ src/js/dom-loader.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Prateek on 3/28/2017.
+ */
+export var secretButton = document.querySelector('#secret-button');
+export var secretParagraph = document.querySelector('#secret-paragraph');
